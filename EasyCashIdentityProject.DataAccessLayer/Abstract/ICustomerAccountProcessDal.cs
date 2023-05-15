d03269379c56d750884e76c091fcf4a2572af431
SenolGunaydin <senol.gunaydin@shopiverse.tech>
2023-05-16 01:01:01 +0300

    *Abstract larin ve manager kisimlarinin olusturulması *Kullanılacak arayüzlerin projeye dahil edilmesi

--- EasyCashIdentityProject.DataAccessLayer/Abstract/ICustomerAccountProcessDal.cs
+++ EasyCashIdentityProject.DataAccessLayer/Abstract/ICustomerAccountProcessDal.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace EasyCashIdentityProject.DataAccessLayer.Abstract
 {
-    public class ICustomerAccountProcessDal : IGenericDal<CustomerAccountProcess>
+    public interface ICustomerAccountProcessDal : IGenericDal<CustomerAccountProcess>
     {
     }
 }
